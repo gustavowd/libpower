9c75db28ed5369c6357f60e7fc83bc272057356d
Gustavo <gustavo.denardin@gmail.com>
2020-03-22 01:09:28 -0300

    Incluido printf

--- libpower.c
+++ libpower.c
@@ -1,8 +1,11 @@
+#include <stdio.h>
+
 float power(float a, float b){
 	int i = 0;
 	int j = (int)b;
 	float resp = a;
 
+	printf("Powerlib usada!\n\r");
 	for(i=0;i<(b-1);i++){
 		resp = resp*a;
 	}
